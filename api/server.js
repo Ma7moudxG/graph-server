00975460f430ebbf1cff179ccc7b07b0df3c9277
Ma7moudxG <132134084+Ma7moudxG@users.noreply.github.com>
2024-11-22 03:49:01 +0400

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,10 +1,15 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
+// Add a test route
+server.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
+});
+
 server.use(router);
 
 module.exports = server;
