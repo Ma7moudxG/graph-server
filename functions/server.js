128f482e921ec766379f2a91cdb5f9790daaf285
Ma7moudxG <132134084+Ma7moudxG@users.noreply.github.com>
2024-11-22 04:45:32 +0400

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -1,9 +1,16 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const path = require("path");
+const low = require("lowdb");
+const Memory = require("lowdb/adapters/Memory");
 
-// Use static JSON file
-const router = jsonServer.router(path.join(__dirname, "../db.json"));
+// Use in-memory adapter
+const adapter = new Memory();
+const db = low(adapter);
+
+// Initialize data
+db.defaults({ posts: [], users: [] }).write();
+
+const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
