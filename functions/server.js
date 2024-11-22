cf77009f897720a510921090487c665f0aaa705e
Ma7moudxG <132134084+Ma7moudxG@users.noreply.github.com>
2024-11-22 05:02:53 +0400

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const path = require("path");
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "../public/db.json")); // Adjust the path if needed
+const router = jsonServer.router(path.join(__dirname, "../db.json")); // Adjust the path if needed
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
