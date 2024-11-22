720c75845b95e72c6540354191da6eb0e463d8a3
Ma7moudxG <132134084+Ma7moudxG@users.noreply.github.com>
2024-11-22 04:09:09 +0400

    Update server.js

--- api/server.js
+++ api/server.js
@@ -4,12 +4,6 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-
-// Add a test route
-server.get("/ping", (req, res) => {
-  res.json({ message: "pong" });
-});
-
 server.use(router);
 
 module.exports = server;
